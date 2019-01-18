0054a47ca1b0d0628b17f5739418f9b04518127e
nazeerd <dudekulanazeerahammad@gmail.com>
2019-01-18 22:58:32 +0530

    using get constructor

--- src/javapractice/example/CreateObject.java
+++ src/javapractice/example/CreateObject.java
@@ -1,5 +1,8 @@
 package javapractice.example;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class CreateObject implements Cloneable{
 
 	public void printMsg(String message) {
@@ -29,6 +32,27 @@ public class CreateObject implements Cloneable{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		try {
+			Constructor<CreateObject> constructor = CreateObject.class.getConstructor();
+			CreateObject c4 = constructor.newInstance();
+		} catch (NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
 		
 	}
 	
